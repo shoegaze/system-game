4a6f1ac7d5c4dfb3fb25692b92059de26527686e
shoegaze <bank.tellr@gmail.com>
2022-08-08 08:07:22 +0900

    Adds deceleration clamping

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,10 +1,14 @@
+using System;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
   [SerializeField] private new Camera camera;
-  [SerializeField, Min(0f)] private float speed;
+  [SerializeField, Min(0f)] private float maxSpeed = 10;
+  [SerializeField, Min(0f)] private float acceleration = 14;
+  [SerializeField, Min(0f)] private float deceleration = 36;
   
   private Rigidbody rb;
+  private Vector3 dir;
   
   private void Awake() {
     rb = GetComponent<Rigidbody>();
@@ -15,7 +19,24 @@ public class PlayerMove : MonoBehaviour {
     float v = Input.GetAxis("Vertical");
     var ct = camera.transform;
     
-    var dir = (h * ct.right + v * ct.forward).normalized;
-    rb.velocity = speed * dir;
+    dir = (h * ct.right + v * ct.forward).normalized;
+  }
+
+  private void FixedUpdate() {
+    var vel = rb.velocity;
+
+    if (Mathf.Approximately(dir.sqrMagnitude, 0f)) {
+      float d = Mathf.Min(deceleration, vel.magnitude /  Time.fixedDeltaTime);
+      
+      var velXY = new Vector3(vel.x, 0f, vel.z);
+      var back = -velXY.normalized;
+      
+      rb.AddForce(d * back, ForceMode.Acceleration);
+    }
+    else {
+      rb.AddForce(acceleration * dir, ForceMode.Acceleration);
+    }
+
+    rb.velocity = Vector3.ClampMagnitude(vel, maxSpeed);
   }
 }
